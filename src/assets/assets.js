e56ac2655a62000d47e0abab48a939ec9fa59e10
MdMostakimBillah <mostakimbillahn609@gmail.com>
2024-08-30 16:35:55 +0600

    responsive Navigation done!

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,3 +1,4 @@
 import logo from "./logo.png";
-
+import sampleLogo from "./hand-keep-book-read-source-600nw-1127076767.webp";
 export const mainLogo = logo;
+export const sample_Logo = sampleLogo;
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,10 +1,16 @@
 import classes from "./style/Navigation.module.css";
 import { MidleNavList, RightNav } from "../content/index";
 import { mainLogo } from "../assets/assets";
+import { useState } from "react";
+
 const Navigation = () => {
+  const [isActive, setIsActive] = useState(false);
+  const toggleClass = () => {
+    setIsActive(!isActive);
+  };
   return (
     <>
-      <nav className="">
+      <nav>
         <div className={`container_page flex-between`}>
           {/* logo  */}
           <div className={classes.logo}>
@@ -12,6 +18,45 @@ const Navigation = () => {
               <img src={mainLogo} alt="logo" />
             </a>
           </div>
+
+          {/* responsive nav  */}
+          <div
+            className={`${isActive ? classes.active : ""}  ${
+              classes.responsiveNavList
+            }`}
+          >
+            {/* middle nav list  */}
+            <div className={classes.middleList}>
+              <ul className={classes.mainNavList}>
+                {MidleNavList.map((item) => (
+                  <li key={item.title}>
+                    <a href="#">{item.title}</a>
+
+                    {item.submenu.length > 0 && (
+                      <ul className={classes.submenuList}>
+                        {item.submenu.map((item) => (
+                          <li key={item}>
+                            <a href="#">{item}</a>
+                          </li>
+                        ))}
+                      </ul>
+                    )}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            {/* login and registration  */}
+            <div className={classes.loginRegistration}>
+              <ul>
+                {RightNav.map((item) => (
+                  <li key={item}>
+                    <a href="#">{item}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+
           {/* middle nav list  */}
           <div className={classes.middleList}>
             <ul className={classes.mainNavList}>
@@ -41,6 +86,11 @@ const Navigation = () => {
                 </li>
               ))}
             </ul>
+          </div>
+
+          {/* toggle bar  */}
+          <div onClick={toggleClass} className={classes.toggleBar}>
+            <span className="material-symbols-outlined">menu</span>
           </div>
         </div>
       </nav>
