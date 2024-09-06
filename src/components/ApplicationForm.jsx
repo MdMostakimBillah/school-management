771e33eb1cbee3151b810dce56d46b8a82c21cb1
MdMostakimBillah <mostakimbillahn609@gmail.com>
2024-09-06 18:26:07 +0600

    application form input all state add and address propblem operation runnig..

--- src/components/ApplicationForm.jsx
+++ src/components/ApplicationForm.jsx
@@ -1,20 +1,73 @@
+import { useState } from "react";
 import classes from "./style/ApplicationForm.module.css";
-
+import { country } from "../content/index";
 const ApplicationForm = () => {
+  // const [country, setCountry] = useState([]);
+  const [distric, setDistric] = useState([]);
+  const [upazilas, setUpazilas] = useState([]);
+  const [postOffice, setPostOffice] = useState([]);
+
+  console.log(country);
+
+  const [formData, setFormData] = useState({
+    studentFirstName: "",
+    studentLastName: "",
+    dateOfBirth: "",
+    BirthSertificateNumber: "",
+    fatherName: "",
+    motherName: "",
+    fatherIDNumber: "",
+    motherIDNumber: "",
+    absenceParentName: "",
+    absenceParentIDNumber: "",
+    zipCode: "",
+    previousInstitution: "",
+    email: "",
+    phoneNumber: "",
+    password: "",
+    confirmPassword: "",
+    nationality: "",
+    gendar: "",
+    selectedDivision: "",
+    selectedDistricts: "",
+  });
+  const inputHandler = (e) => {
+    const { name, value } = e.target;
+    setFormData((previousData) => ({
+      ...previousData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData); // Process the form data
+  };
+
   return (
     <div className={classes.applicationFormWraper}>
       <h1>আবেদন ফরম পূরণ করুন</h1>
 
-      <form action="#">
+      <form action="#" onSubmit={handleSubmit}>
         {/* name  */}
         <div className={classes.singleLine}>
           <div className={classes.firtsInpur}>
             <label htmlFor="">প্রথম নাম:</label>
-            <input type="text" />
+            <input
+              name="studentFirstName"
+              type="text"
+              value={formData.studentFirstName}
+              onChange={inputHandler}
+            />
           </div>
           <div className={classes.SecondInpur}>
             <label htmlFor="">শেষ নাম:</label>
-            <input type="text" />
+            <input
+              name="studentLastName"
+              type="text"
+              value={formData.studentLastName}
+              onChange={inputHandler}
+            />
           </div>
         </div>
 
@@ -22,13 +75,23 @@ const ApplicationForm = () => {
         <div className={classes.singleLine}>
           <div className={classes.firtsInpur}>
             <label htmlFor="">জন্ম তারিখ:</label>
-            <input type="date" />
+            <input
+              name="dateOfBirth"
+              type="date"
+              value={formData.dateOfBirth}
+              onChange={inputHandler}
+            />
           </div>
           <div className={classes.SecondInpur}>
             <label htmlFor="">জাতীয়তা:</label>
-            <select name="" id="">
-              <option value="">বাংলাদেশী</option>
-              <option value="">অন্যান্য</option>
+            <select
+              name="nationality"
+              value={formData.nationality}
+              onChange={inputHandler}
+            >
+              <option value="">জাতীয়তা নির্ধারণ করুন-</option>
+              <option value="বাংলাদেশী">বাংলাদেশী</option>
+              <option value="অন্যান্য">অন্যান্য</option>
             </select>
           </div>
         </div>
@@ -36,11 +99,21 @@ const ApplicationForm = () => {
         <div className={`${classes.singleLine} ${classes.DateOfBirthIdentity}`}>
           <div className={classes.firtsInpur}>
             <label htmlFor="">জন্ম সনদ নাম্বার:</label>
-            <input type="number" />
+            <input
+              name="BirthSertificateNumber"
+              type="number"
+              value={formData.BirthSertificateNumber}
+              onChange={inputHandler}
+            />
           </div>
           <div className={classes.SecondInpur}>
             <label htmlFor="">লিঙ্গ:</label>
-            <select name="" id="">
+            <select
+              name="gendar"
+              value={formData.gendar}
+              onChange={inputHandler}
+            >
+              <option value="">লিঙ্গ নির্ধারণ করুন-</option>
               <option value="male">পুরুষ</option>
               <option value="female">নারী</option>
               <option value="other">অন্যান্য</option>
@@ -52,22 +125,66 @@ const ApplicationForm = () => {
         <div className={classes.singleLine}>
           <div className={classes.firtsInpur}>
             <label htmlFor="">পিতার নাম:</label>
-            <input type="text" />
+            <input
+              name="fatherName"
+              type="text"
+              value={formData.fatherName}
+              onChange={inputHandler}
+            />
           </div>
           <div className={classes.SecondInpur}>
             <label htmlFor="">মাতার নাম:</label>
-            <input type="text" />
+            <input
+              name="motherName"
+              type="text"
+              value={formData.motherName}
+              onChange={inputHandler}
+            />
           </div>
         </div>
         {/* baba mayer id number  */}
         <div className={classes.singleLine}>
           <div className={classes.firtsInpur}>
             <label htmlFor="">পিতার ভোটার আইডি নাম্বার:</label>
-            <input type="number" />
+            <input
+              name="fatherIDNumber"
+              type="text"
+              value={formData.fatherIDNumber}
+              onChange={inputHandler}
+            />
           </div>
           <div className={classes.SecondInpur}>
             <label htmlFor="">মাতার ভোটার আইডি নাম্বার:</label>
-            <input type="number" />
+            <input
+              name="motherIDNumber"
+              type="number"
+              value={formData.motherIDNumber}
+              onChange={inputHandler}
+            />
+          </div>
+        </div>
+
+        {/* baba mayer obortomane jini acen  */}
+        <div className={classes.singleLine}>
+          <div className={classes.firtsInpur}>
+            <label htmlFor="">পিতা-মাতার অবর্তমানে যিনি আছেন তার নাম:</label>
+            <input
+              name="absenceParentName"
+              type="text"
+              value={formData.absenceParentName}
+              onChange={inputHandler}
+            />
+          </div>
+          <div className={classes.SecondInpur}>
+            <label htmlFor="">
+              পিতা-মাতার অবর্তমানে যিনি আছেন তার ভোটার আইডি নাম্বার:
+            </label>
+            <input
+              name="absenceParentIDNumber"
+              type="number"
+              value={formData.absenceParentIDNumber}
+              onChange={inputHandler}
+            />
           </div>
         </div>
 
@@ -75,20 +192,39 @@ const ApplicationForm = () => {
         <div className={classes.singleLine}>
           <div className={classes.firtsInpur}>
             <label htmlFor="">বিভাগ:</label>
-            <select name="" id="">
-              <option value="">জাতীয়তা:</option>
-              <option value="">জাতীয়তা:</option>
-              <option value="">জাতীয়তা:</option>
-              <option value="">জাতীয়তা:</option>
+            <select
+              name="selectedDivision"
+              value={formData.selectedDivision}
+              onChange={inputHandler}
+            >
+              <option value="">বিভাগ নির্ধারণ করুন-</option>
+              {/* {country.map((item, inex) =>
+                item.name && formData.nationality
+                  ? item.division.map((allDivision, index) => (
+                      <option key={index} value={allDivision.name}>
+                        {allDivision.name}
+                      </option>
+                    ))
+                  : " "
+              )} */}
             </select>
           </div>
           <div className={classes.SecondInpur}>
             <label htmlFor="">জেলা:</label>
-            <select name="" id="">
-              <option value="">জাতীয়তা:</option>
-              <option value="">জাতীয়তা:</option>
-              <option value="">জাতীয়তা:</option>
-              <option value="">জাতীয়তা:</option>
+            <select
+              name="selectedDistricts"
+              value={formData.selectedDistricts}
+              onChange={inputHandler}
+            >
+              <option value="">জেলা নির্ধারণ করুন- </option>
+              {/* {country.map((item, inex) =>
+                item.name && formData.nationality
+                  ? item.division.map((allDivision, index) =>
+                      console.log(allDivision.name && formData.selectedDivision)
+                    )
+                  : " "
+              )} */}
+              {/* {console.log(formData.selectedDivision)} */}
             </select>
           </div>
         </div>
@@ -107,7 +243,10 @@ const ApplicationForm = () => {
           <div className={classes.SecondInpur}>
             <label htmlFor="">পোষ্ট অফিস:</label>
             <select name="" id="">
-              <input type="text" />
+              <option value="">জাতীয়তা:</option>
+              <option value="">জাতীয়তা:</option>
+              <option value="">জাতীয়তা:</option>
+              <option value="">জাতীয়তা:</option>
             </select>
           </div>
         </div>
@@ -115,9 +254,12 @@ const ApplicationForm = () => {
           {/* zip code */}
           <div className={classes.SecondInpur}>
             <label htmlFor="">জিপ কোড:</label>
-            <select name="" id="">
-              <input type="number" />
-            </select>
+            <input
+              name="zipCode"
+              type="number"
+              value={formData.zipCode}
+              onChange={inputHandler}
+            />
           </div>
         </div>
 
@@ -125,7 +267,12 @@ const ApplicationForm = () => {
         <div className={`${classes.singleLine} ${classes.previouScholl}`}>
           <div className={classes.firtsInpur}>
             <label htmlFor="">পূর্বের প্রতিষ্ঠানের নাম:</label>
-            <input type="text" />
+            <input
+              name="previousInstitution"
+              type="text"
+              value={formData.previousInstitution}
+              onChange={inputHandler}
+            />
           </div>
           <div className={classes.SecondInpur}>
             <label htmlFor="">শ্রেণী:</label>
@@ -169,19 +316,34 @@ const ApplicationForm = () => {
           {/* mobile number  */}
           <div className={classes.SecondInpur}>
             <label htmlFor="">মোবাইল নাম্বার:</label>
-            <input type="number" />
+            <input
+              name="email"
+              type="number"
+              value={formData.email}
+              onChange={inputHandler}
+            />
           </div>
         </div>
         <div className={classes.singleLine}>
           {/* password  */}
           <div className={classes.firtsInpur}>
             <label htmlFor="">পাসওয়ার্ড:</label>
-            <input type="password" />
+            <input
+              name="password"
+              type="password"
+              value={formData.password}
+              onChange={inputHandler}
+            />
           </div>
           {/* confirm password */}
           <div className={classes.SecondInpur}>
             <label htmlFor="">পুনরায় আবার পাসওয়ার্ড দিন:</label>
-            <input type="password" />
+            <input
+              name="confirmPassword"
+              type="password"
+              value={formData.confirmPassword}
+              onChange={inputHandler}
+            />
           </div>
         </div>
 
@@ -193,7 +355,7 @@ const ApplicationForm = () => {
           </div>
         </div>
         <div className={classes.submitButton}>
-          <button>নিশ্চিত করুন</button>
+          <button type="submit">নিশ্চিত করুন</button>
         </div>
       </form>
     </div>
